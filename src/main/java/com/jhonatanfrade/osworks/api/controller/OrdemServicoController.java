4a973e4c208f857a52beff7ad88f765ae646582b
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-10-22 16:07:52 -0300

    representation model de ordens de servico

--- src/main/java/com/jhonatanfrade/osworks/api/controller/OrdemServicoController.java
+++ src/main/java/com/jhonatanfrade/osworks/api/controller/OrdemServicoController.java
@@ -2,13 +2,17 @@ package com.jhonatanfrade.osworks.api.controller;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import com.jhonatanfrade.osworks.api.model.OrdemServicoInput;
+import com.jhonatanfrade.osworks.api.model.OrdemServicoModel;
 import com.jhonatanfrade.osworks.domain.model.OrdemServico;
 import com.jhonatanfrade.osworks.domain.repositories.OrdemServicoRepository;
 import com.jhonatanfrade.osworks.domain.service.GestaoOrdemServicoService;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,25 +34,44 @@ public class OrdemServicoController {
   @Autowired
   private OrdemServicoRepository ordemServicoRespository;
 
+  @Autowired
+  private ModelMapper modelMapper;
+
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public OrdemServico criar(@Valid @RequestBody OrdemServico ordemServico) {
-    return gestaoOrdemServico.criar(ordemServico);
+  public OrdemServicoModel criar(@Valid @RequestBody OrdemServicoInput ordemServicoInput) {
+    OrdemServico ordemServico = toEntity(ordemServicoInput);
+    return toModel(gestaoOrdemServico.criar(ordemServico));
   }
 
   @GetMapping
-  public List<OrdemServico> listar() {
-    return ordemServicoRespository.findAll();
+  public List<OrdemServicoModel> listar() {
+    return toCollectionModel(ordemServicoRespository.findAll());
   }
 
   @GetMapping("/{ordemServicoId}")
-  public ResponseEntity<OrdemServico> buscar(@PathVariable Long ordemServicoId) {
+  public ResponseEntity<OrdemServicoModel> buscar(@PathVariable Long ordemServicoId) {
     Optional<OrdemServico> ordemServico = ordemServicoRespository.findById(ordemServicoId);
 
     if(ordemServico.isPresent()) {
-      return ResponseEntity.ok(ordemServico.get());
+      OrdemServicoModel ordemServicoModel = toModel(ordemServico.get());
+      return ResponseEntity.ok(ordemServicoModel);
     }
 
     return ResponseEntity.notFound().build();
+  }
+
+  private OrdemServicoModel toModel(OrdemServico ordemServico) {
+    return modelMapper.map(ordemServico, OrdemServicoModel.class);
+  }
+
+  private List<OrdemServicoModel> toCollectionModel(List<OrdemServico> ordensServico) {
+    return ordensServico.stream()
+      .map(ordemServico -> toModel(ordemServico))
+      .collect(Collectors.toList());
+  }
+
+  private OrdemServico toEntity(OrdemServicoInput ordemServicoInput) {
+    return modelMapper.map(ordemServicoInput, OrdemServico.class);
   }
 }
