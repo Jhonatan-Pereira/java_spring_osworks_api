53658e3effb325252c20babecee5d7e5ba50f712
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-10-21 23:54:41 -0300

    created ClienteRepository

--- src/main/java/com/jhonatanfrade/osworks/api/controller/ClienteController.java
+++ src/main/java/com/jhonatanfrade/osworks/api/controller/ClienteController.java
@@ -1,29 +1,22 @@
 package com.jhonatanfrade.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import com.jhonatanfrade.osworks.domain.model.Cliente;
+import com.jhonatanfrade.osworks.domain.repositories.ClienteRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClienteController {
+
+  @Autowired
+  private ClienteRepository clienteRepository;
   
   @GetMapping("/clientes")
   public List<Cliente> listar() {
-    var cliente1 = new Cliente();
-    cliente1.setId(1L);
-    cliente1.setNome("João das neves");
-    cliente1.setTelefone("15975345685");
-    cliente1.setEmail("joao@uol.com");
-    var cliente2 = new Cliente();
-    cliente2.setId(2L);
-    cliente2.setNome("Maria");
-    cliente2.setTelefone("45685245815");
-    cliente2.setEmail("maria@uol.com");
-
-    return Arrays.asList(cliente1, cliente2);
+    return clienteRepository.findAll();
   }
 }
